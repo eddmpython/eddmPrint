4b988cd6b988f3cc229622c872adfcd077f31df2
eddmpython <o12486vs2@gmail.com>
2025-04-03 18:39:21 +0900

    주석 수정

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
 from setuptools import setup, find_packages
+import os
+
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name="eddmPrint",
@@ -8,7 +14,7 @@ setup(
     author="eddmpython",
     author_email="eddmpython@gmail.com",
     description="색상과 위치 정보가 포함된 개선된 프린트 라이브러리",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/eddmpython/eddmPrint",
     classifiers=[
