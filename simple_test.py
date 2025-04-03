663ab31730ff0a0ada427c82acd059ab8f5dedb8
eddmpython <o12486vs2@gmail.com>
2025-04-03 16:30:18 +0900

    20250403_first

--- simple_test.py
+++ simple_test.py
@@ -1,12 +1,1 @@
-import sys
-sys.path.append('src')
-
-# 기본 프린트
-print("1. 기본 프린트 테스트")
-
-# 색상 코드 임포트 및 테스트
-from eddmPrint.colors import Colors
-print(f"{Colors.RED}2. 빨간색 텍스트{Colors.RESET}")
-
-# 완료 메시지
-print("테스트 완료") 
+ 
---
+++ test.py
@@ -0,0 +1,8 @@
+from eddmPrint import EddmPrint
+
+printer = EddmPrint()
+printer.start()
+print("Hello, World!")
+printer.restore()
+print("Hello, World!")
+
